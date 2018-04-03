08058489948a5f028f6b7826f792f0c04f8b2133
Yanis <mejias1405@gmail.com>
2018-04-02 20:27:53 -0430

    CAMBIO EN HOME SECCION COMO JUGAR

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -187,7 +187,8 @@
 
         </div>
       </div>
-    </section><!-- #featured-services -->
+    </section>
+    <!-- #featured-services -->
 
     <!--==========================
       About Us Section
@@ -196,8 +197,8 @@
       <div class="container">
 
         <header class="section-header">
-          <h3>About Us</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          <h3>Como jugar Quinielas con xportgold...</h3>
+          
         </header>
 
         <div class="row about-cols">
@@ -205,12 +206,14 @@
           <div class="col-md-4 wow fadeInUp">
             <div class="about-col">
               <div class="img">
-                <img src="img/about-mission.jpg" alt="" class="img-fluid">
-                <div class="icon"><i class="ion-ios-speedometer-outline"></i></div>
+                <img src="img/about_registro.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-list-outline"></i></div>
               </div>
-              <h2 class="title"><a href="#">Our Mission</a></h2>
+              <h2 class="title"><a href="#">PASO 1:</a></h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               Registra de forma rápida y sencilla tus datos basicos y disfruta la experiencia de ser un usuario exclusivo de xportgold.
+				
+				 
               </p>
             </div>
           </div>
@@ -218,12 +221,12 @@
           <div class="col-md-4 wow fadeInUp" data-wow-delay="0.1s">
             <div class="about-col">
               <div class="img">
-                <img src="img/about-plan.jpg" alt="" class="img-fluid">
+                <img src="img/about_quiniela.jpg" alt="" class="img-fluid">
                 <div class="icon"><i class="ion-ios-list-outline"></i></div>
               </div>
-              <h2 class="title"><a href="#">Our Plan</a></h2>
+              <h2 class="title"><a href="#">PASO 2</a></h2>
               <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                Demuestra tus conocimientos y habilidades en el deporte rey pronosticando los resultados de los partidos del mundial Rusia 2018.
               </p>
             </div>
           </div>
@@ -231,12 +234,12 @@
           <div class="col-md-4 wow fadeInUp" data-wow-delay="0.2s">
             <div class="about-col">
               <div class="img">
-                <img src="img/about-vision.jpg" alt="" class="img-fluid">
-                <div class="icon"><i class="ion-ios-eye-outline"></i></div>
+                <img src="img/about_ganar.jpg" alt="" class="img-fluid">
+                <div class="icon"><i class="ion-ios-list-outline"></i></div>
               </div>
-              <h2 class="title"><a href="#">Our Vision</a></h2>
+              <h2 class="title"><a href="#">Paso 3</a></h2>
               <p>
-                Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+               .... Y Gana el fabuloso "GOLD POT" que contiene las cantidades de apuestas de miles de participantes de nuestra quiniela general...
               </p>
             </div>
           </div>
@@ -244,12 +247,13 @@
         </div>
 
       </div>
-    </section><!-- #about -->
+    </section>
+    <!-- #about -->
 
     <!--==========================
       Services Section
     ============================-->
-    <section id="services">
+     <!--<section id="services">
       <div class="container">
 
         <header class="section-header wow fadeInUp">
@@ -293,23 +297,25 @@
         </div>
 
       </div>
-    </section><!-- #services -->
+    </section>-->
+    <!-- #services -->
 
     <!--==========================
       Call To Action Section
     ============================-->
-    <section id="call-to-action" class="wow fadeIn">
+     <!--<section id="call-to-action" class="wow fadeIn">
       <div class="container text-center">
         <h3>Call To Action</h3>
         <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <a class="cta-btn" href="#">Call To Action</a>
       </div>
-    </section><!-- #call-to-action -->
+    </section>-->
+    <!-- #call-to-action -->
 
     <!--==========================
       Skills Section
     ============================-->
-    <section id="skills">
+     <!--<section id="skills">
       <div class="container">
 
         <header class="section-header">
@@ -346,12 +352,12 @@
         </div>
 
       </div>
-    </section>
+    </section>-->
 
     <!--==========================
       Facts Section
     ============================-->
-    <section id="facts"  class="wow fadeIn">
+     <!--<section id="facts"  class="wow fadeIn">
       <div class="container">
 
         <header class="section-header">
@@ -388,7 +394,8 @@
         </div>
 
       </div>
-    </section><!-- #facts -->
+    </section>-->
+    <!-- #facts -->
 
     <!--==========================
       Portfolio Section
@@ -551,12 +558,13 @@
         </div>
 
       </div>
-    </section><!-- #portfolio -->
+    </section>
+    <!-- #portfolio -->
 
     <!--==========================
       Clients Section
     ============================-->
-    <section id="clients" class="wow fadeInUp">
+     <!--<section id="clients" class="wow fadeInUp">
       <div class="container">
 
         <header class="section-header">
@@ -575,12 +583,13 @@
         </div>
 
       </div>
-    </section><!-- #clients -->
+    </section>-->
+    <!-- #clients -->
 
     <!--==========================
-      Clients Section
+      Clients Section testimonials
     ============================-->
-    <section id="testimonials" class="section-bg wow fadeInUp">
+    <!--<section id="testimonials" class="section-bg wow fadeInUp">
       <div class="container">
 
         <header class="section-header">
@@ -647,12 +656,13 @@
         </div>
 
       </div>
-    </section><!-- #testimonials -->
+    </section>-->
+    <!-- #testimonials -->
 
     <!--==========================
       Team Section
     ============================-->
-    <section id="team">
+     <!--<section id="team">
       <div class="container">
         <div class="section-header wow fadeInUp">
           <h3>Team</h3>
@@ -736,7 +746,8 @@
         </div>
 
       </div>
-    </section><!-- #team -->
+    </section>
+    <!-- #team -->
 
     
 
