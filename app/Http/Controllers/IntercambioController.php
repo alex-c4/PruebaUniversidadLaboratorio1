179024d85c6f693b08750e9f94fe0a30756887eb
abraham <abraham19@gmail.com>
2018-05-09 01:12:55 -0400

    validacion

--- app/Http/Controllers/IntercambioController.php
+++ app/Http/Controllers/IntercambioController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+ 
+use App\User;
+use App\sticker;
 use App\Intercambio;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -32,21 +34,31 @@ class IntercambioController extends Controller
 
 		public function store($id_solicitante,$id_sticker){
         //$this->validator(request()->all())->validate();  es necesario validar datos??
-        $intercambio=Intercambio::where('id_barajita',$id_sticker)->where('id_usuario_solicitante',$id_solicitante)->first();    
+        $id_solicitante=User::Where('id',$id_solicitante)->value('id');
+        $id_sticker=Sticker::Where('id',$id_sticker)->value('id');
 
-        if ($intercambio==null){//pendiente validar  respuesta del query antes de pasar respuesta a la ruta
-            $respuesta = Intercambio::create([
-                'id_barajita' => $id_sticker,
-                'id_usuario_solicitante' => $id_solicitante,
-                'estatus' => 'EN PROCESO'
-            ])->id;
+        //return'id_solicitante'.$id_solicitante.' id_sticker: '.$id_sticker;
 
-            return redirect()->route('mensajeria.create',['id_intercambio'=>$respuesta]);
+        //se valida que el solicitante y elstiker aintercambiar exitan
+        if ($id_solicitante == null or $id_sticker == null){
+           return 'los datos sumbinistrados no son correctos verifique';
+
         }else{
-            // si ya existe un intercambio para este usuario y sticker;
-            return redirect()->route('conversacion.mostrar',['id_intercambio'=>$intercambio->id_intercambio]);
+            $intercambio=Intercambio::where('id_barajita',$id_sticker)->where('id_usuario_solicitante',$id_solicitante)->first();
+
+            if ($intercambio==null){//pendiente validar  respuesta del query antes de pasar respuesta a la ruta
+                $respuesta = Intercambio::create([
+                    'id_barajita' => $id_sticker,
+                    'id_usuario_solicitante' => $id_solicitante,
+                    'estatus' => 'EN PROCESO'
+                ])->id;
+
+                return redirect()->route('mensajeria.create',['id_intercambio'=>$respuesta]);
+            }else{
+                // si ya existe un intercambio para este usuario y sticker;
+                return redirect()->route('conversacion.mostrar',['id_intercambio'=>$intercambio->id_intercambio]);
+            }
         }
-        
     }
 
 
