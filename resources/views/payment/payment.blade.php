cbd25e81273a17003b9f705f554e267f3416653b
Alex Penaloza <alexdaniel2601@gmail.com>
2018-06-10 13:10:51 -0400

    se crea link para ver reglas del juego

--- resources/views/payment/payment.blade.php
+++ resources/views/payment/payment.blade.php
@@ -182,7 +182,7 @@
                     </button>
                   </div>
                   <div class="modal-body" style="font-size: 12px;">
-                    <p class="c3"><span class="c5">Acepto y entiendo el pago exigido por <b>XportGold</b> a través de <b>PayPal</b> explicado en su reglamento del Juego online a través de la página Web <a class="c22" href="{{ url('/') }}">www.xportgold.com</a> y bajo ningún concepto será motivo de reintegro, devolución o reclamación. Declino cualquier posibilidad de abrir una disputa ante el vendedor <a class="c22" href="{{ url('/') }}">www.xportgold.com</a> e igualmente que esta operación es efectuada por mí y no por un tercero.</span></p>
+                    <p class="c3"><span class="c5">Acepto y entiendo el pago exigido por <b>XportGold</b> a través de <b>PayPal</b> explicado en su <a href="{{ asset('/files/Instructivo Quinielas XportGold.pdf') }}" target="_blank">reglamento del Juego online</a>  a través de la página Web <a class="c22" href="{{ url('/') }}">www.xportgold.com</a> y bajo ningún concepto será motivo de reintegro, devolución o reclamación. Declino cualquier posibilidad de abrir una disputa ante el vendedor <a class="c22" href="{{ url('/') }}">www.xportgold.com</a> e igualmente que esta operación es efectuada por mí y no por un tercero.</span></p>
                     
                     <p class="c7 c4"><span class="c2"></span></p><p class="c3"><span class="c5">Al celebrar este acuerdo, ratificas que XportGold es marca propiedad de Y3A Tecnology C.A</span></p>
                     
