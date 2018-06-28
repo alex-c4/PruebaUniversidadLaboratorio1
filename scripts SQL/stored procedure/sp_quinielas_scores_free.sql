2a502a724149ab3376b206aabdf9fb509adb1c0d
Yanis <mejias1405@gmail.com>
2018-06-28 01:05:40 -0430

    sincronizacion 27 de junio yanis modificacion home

--- scripts SQL/stored procedure/sp_quinielas_scores_free.sql
+++ scripts SQL/stored procedure/sp_quinielas_scores_free.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_quinielas_scores_free`(IN `quiniela_id` INT)
+CREATE PROCEDURE `sp_quinielas_scores_free`(IN `quiniela_id` INT)
     NO SQL
 BEGIN
 
@@ -12,11 +12,11 @@ sum(`pro`.`pronostic_score`) AS `puntos`,
 `q`.`id_quiniela` AS `id_quiniela` 
 
 FROM 
-((((`xportgoldbd_pruebas`.`pronostics` `pro`
- join `xportgoldbd_pruebas`.`bets` `be` on((`pro`.`bet_id` = `be`.`id`)))
- join `xportgoldbd_pruebas`.`users` `us` on((`be`.`id_user` = `us`.`id`)))
- join `xportgoldbd_pruebas`.`quinielas` `q` on((`q`.`id_quiniela` = `be`.`id_quiniela`))) 
-join `xportgoldbd_pruebas`.`games` `ga` on((`ga`.`id` = `pro`.`id_game`))) 
+((((`xportgol_bd_pruebas`.`pronostics` `pro`
+ join `xportgol_bd_pruebas`.`bets` `be` on((`pro`.`bet_id` = `be`.`id`)))
+ join `xportgol_bd_pruebas`.`users` `us` on((`be`.`id_user` = `us`.`id`)))
+ join `xportgol_bd_pruebas`.`quinielas` `q` on((`q`.`id_quiniela` = `be`.`id_quiniela`))) 
+join `xportgol_bd_pruebas`.`games` `ga` on((`ga`.`id` = `pro`.`id_game`))) 
 
 WHERE 
 (`be`.`verification` != '1')
