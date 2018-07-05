daba9756542e85eb31a3d263c5992da1faee41c4
Alex Penaloza <alexdaniel2601@gmail.com>
2018-07-05 15:41:12 -0400

    actualizacion de SP y codigo para crear nueva tabla activphase

--- scripts SQL/stored procedure/sp_getMyPronosticsDetails.sql
+++ scripts SQL/stored procedure/sp_getMyPronosticsDetails.sql
@@ -14,11 +14,17 @@ BEGIN
         p.pronostic_club_1,
         (SELECT nombre FROM clubs WHERE id_club = g.id_club_2) AS nombre_club_2,
         p.pronostic_club_2,
-        p.id AS pronostic_id
-
+        p.id AS pronostic_id,
+		ap.phase
     FROM pronostics AS p
         INNER JOIN games AS g ON g.id=p.id_game
-    WHERE p.bet_id = betID
+        INNER JOIN quinielas AS q ON q.id_quiniela=p.id_quiniela
+        INNER JOIN activephases AS ap ON ap.championship_id = g.id_champ
+    WHERE 
+    	p.bet_id = betID AND
+        q.isActive = true AND
+        ap.phase = g.fase
+        
     ORDER BY g.date ASC;
 END$$
 DELIMITER ;
