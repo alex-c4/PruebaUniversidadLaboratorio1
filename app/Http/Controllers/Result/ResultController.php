260904239ce0cc9a0033c0b3942cc57728d8e9bb
Yanis <mejias1405@gmail.com>
2018-07-05 22:55:13 -0430

    modificacion del result para cargas los juegos de cuartos

--- app/Http/Controllers/Result/ResultController.php
+++ app/Http/Controllers/Result/ResultController.php
@@ -103,7 +103,7 @@ class ResultController extends Controller
 
             $fase='ELIMINATORIAS';
         }else{
-            $fase='octavos';
+            $fase='cuartos';
         }       
 
        
